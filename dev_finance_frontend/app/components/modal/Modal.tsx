57988c719bafbb5a5d24f8386963dd7fcdbd4110
Thiago Seixas <ticolls@hotmail.com>
2023-06-02 13:09:44 -0300

    setting cors config and starting to post data

--- dev_finance_frontend/app/components/modal/Modal.tsx
+++ dev_finance_frontend/app/components/modal/Modal.tsx
@@ -1,9 +1,38 @@
+'use client'
+
 import { useModal } from '@/app/hooks/useModal'
 import './modal.css'
+import { FormEvent, useState } from 'react'
 
 export function Modal() {
 
     const { setModalStatus } = useModal()
+
+    const [description, setDescription] = useState<String | null>()
+    const [amount, setamount] = useState<Number | null>()
+    const [date, setDate] = useState<String | null>()
+
+    function handleDescription(e: FormEvent<HTMLInputElement>) {
+        setDescription(e.currentTarget.value)
+    }
+
+    function handleAmount(e: FormEvent<HTMLInputElement>) {
+        setamount(Number(e.currentTarget.value))
+    }
+
+    function handleDate(e: FormEvent<HTMLInputElement>) {
+        setDate(e.currentTarget.value)
+    }
+
+    async function handleSubmitForm() {
+        const res = await fetch('http://localhost:8080/transaction', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ description, amount, date }),
+        })
+    }
 
     return (
         <div className="modal-overlay">
@@ -13,24 +42,24 @@ export function Modal() {
                     <form>
                         <div className="input-group">
                             <label className="sr-only" htmlFor="description">Descrição</label>
-                            <input type="text" className="description" name="description" placeholder="Descrição" />
+                            <input type="text" className="description" name="description" placeholder="Descrição" onChange={handleDescription} />
                         </div>
 
                         <div className="input-group">
                             <label className="sr-only" htmlFor="amount">Valor</label>
-                            <input type="number" step="0.01" className="amount" name="amount" placeholder="0,00" />
+                            <input type="number" step="0.01" className="amount" name="amount" placeholder="0,00" onChange={handleAmount} />
                             <small className="help">Use o sinal - (negativo) para despesas
                                 e , (vírgula) para casas decimais</small>
                         </div>
 
                         <div className="input-group">
                             <label className="sr-only" htmlFor="date">Data</label>
-                            <input type="date" className="date" name="date" placeholder="Descrição" />
+                            <input type="date" className="date" name="date" placeholder="Descrição" onChange={handleDate} />
                         </div>
 
                         <div className="input-group actions">
                             <a className="button cancel" onClick={() => setModalStatus(false)}>Cancelar</a>
-                            <a className="save-button">Salvar</a>
+                            <a className="save-button" onClick={handleSubmitForm}>Salvar</a>
                         </div>
                     </form>
                 </div>
